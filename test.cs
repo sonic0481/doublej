93c4fcd79933eed02d2d69c3c68b98fa54170b5f
USER-PC\yeongbin.Kim <sonic0481@gmail.com>
2017-03-09 10:41:06 +0900

    no message

---
+++ test.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+	private string 		test_s;
+	// Use this for initialization
+	void Start ()
+	{
+		test_s = "test";
+		Debug.Log (test_s);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
